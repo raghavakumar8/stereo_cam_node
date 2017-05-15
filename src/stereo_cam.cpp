2aab4eed0a1e09148721726c67ca7083ee67a2fd
Raghava Kumar <raghava.kumar8@gmail.com>
2017-05-15 00:52:03 -0400

    Added simple xillybus read/write

--- src/stereo_cam.cpp
+++ src/stereo_cam.cpp
@@ -5,20 +5,45 @@
 #include <stdlib.h>
 #include <vector>
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termio.h>
+#include <signal.h>
+
 #include "ros/ros.h"
 #include <image_transport/image_transport.h>
 #include "sensor_msgs/LaserScan.h"
 
+#define WIDTH 447
+#define HEIGHT 370
+
+// Global Variables
+int fr, fw;
+
 image_transport::Subscriber im_sub;
 image_transport::Publisher im_pub;
+
+unsigned char wr_buf[WIDTH*HEIGHT];
+unsigned char rd_buf[WIDTH*HEIGHT];
+
+// Function Prototypes
+void allwrite(int fd, unsigned char *buf, int len);
+void allread(int fd, unsigned char *buf, int len);
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg){
 	ROS_INFO("Recieved!");
 	
 	// Send this to the xillybus module
-	for (int i = 0; i < 447*370; i++){
-			
+	for (int i = 0; i < WIDTH*HEIGHT; i++){
+		wr_buf[i] = (* msg).data.data()[i];
 	}
+
+	//allwrite(fw, wr_buf, WIDTH*HEIGHT);
 
 	// Generate Image
 	sensor_msgs::Image img;
@@ -26,18 +51,21 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
 	img.header.stamp = ros::Time::now();
 	img.header.frame_id = "stereo_cam";
 
-	img.height = 370;
-	img.width = 447;
+	img.height = HEIGHT;
+	img.width = WIDTH;
 	
 	img.encoding = "mono8";
 	img.is_bigendian = 0;
 	
-	img.step = 447;
-        img.data = std::vector<uint8_t>(447*370);
+	img.step = WIDTH;
+        img.data = std::vector<uint8_t>(WIDTH*HEIGHT);
 
-	// Fill image by reading from xillybus module	
-	for (int i = 0; i < 447*370; i++){
-		img.data.data()[i] = 255;	
+	// Fill image by reading from xillybus module
+	//allread(fr, rd_buf, WIDTH*HEIGHT);
+
+	for (int i = 0; i < WIDTH*HEIGHT; i++){
+		img.data.data()[i] = 255;
+		//img.data.data()[i] = rd_buf[i];	
 	}
 	
 	im_pub.publish(img);
@@ -48,14 +76,27 @@ int main(int argc, char **argv){
 	ros::NodeHandle n;
 	
 	// Open read/write files 
-	
+	fw = open("/dev/xillybus_write_32", O_WRONLY);
+	fr = open("/dev/xillybus_read_32", O_RDONLY);
+
+	// Error checking
+	if (fw < 0) ROS_INFO("Could not open xillybus_write_32");
+	if (fr < 0) ROS_INFO("Could not open xillybus_read_32");
+
 	image_transport::ImageTransport it(n);
 
 	// Subscribe to camera images, publish stereo images 
 	im_sub = it.subscribe("camera/image", 1, imageCallback);	
 	im_pub = it.advertise("stereo/image", 1);
 
-	ros::Publisher pub = n.advertise<sensor_msgs::LaserScan>("scan",100);
+	// Wait for callbacks!
+	ros::spin();
+
+	// Close read/write files
+	close(fw);
+	close(fr);
+
+	/*ros::Publisher pub = n.advertise<sensor_msgs::LaserScan>("scan",100);
 	ros::Rate loop_rate(50);
 
 	int count = 0;
@@ -99,7 +140,48 @@ int main(int argc, char **argv){
 		loop_rate.sleep();
 		
 		count++;
-	}
+	}*/
+}
 
-	// Close read/write files
+void allwrite(int fd, unsigned char *buf, int len) {
+  int sent = 0;
+  int rc;
+
+  while (sent < len) {
+    rc = write(fd, buf + sent, len - sent);
+
+    if ((rc < 0) && (errno == EINTR))
+      continue;
+
+    if (rc < 0) {
+      perror("allwrite() failed to write");
+      exit(1);
+    }
+
+    if (rc == 0) {
+      fprintf(stderr, "Reached write EOF (?!)\n");
+      exit(1);
+    }
+
+    sent += rc;
+  }
+}
+
+void allread(int fd, unsigned char *buf, int len) {
+  int recd = 0;
+  int rc;
+
+  while (recd < len) {
+    rc = write(fd, buf + recd, len - recd);
+
+    if ((rc < 0) && (errno == EINTR))
+      continue;
+
+    if (rc < 0) {
+      perror("allread() failed to read");
+      exit(1);
+    }
+
+    recd += rc;
+  }
 }
